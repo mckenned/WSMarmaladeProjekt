69557e91ca05fd03ca623d48e22d55084a512eb8
js-who <Jessica.Rother96@gmx.de>
2019-06-15 22:44:13 +0200

    Exchange of normal links with django site links
    e.g.
    <a hre="{% url 'index' %}"></a>

--- marmelade/marmeladenladen/views.py
+++ marmelade/marmeladenladen/views.py
@@ -32,26 +32,4 @@ def start(request):
     return render(request, 'Frontpage.html')
 
 def index(request):
-    # Get every tupel with certain name and change the QuerySet into a Dictionary
-
-    list_ingredients= ['Banana', 'Apple', 'Strawberry', 'Honey']
-    fruit1 = Ingredients.object.filter(Name = list_ingredients[0]).values()[0]
-    fruit2 = Ingredients.object.filter(Name = list_ingredients[1]).values()[0]
-    fruit3 = Ingredients.object.filter(Name = list_ingredients[2]).values()[0]
-    spice = Ingredients.object.filter(Name = list_ingredients[3]).values()[0]
-
-    # Save the name of the fruit into a variable
-    name_fruit1 = fruit1["Name"].lower()
-    name_fruit2 = fruit2["Name"].lower()
-    name_fruit3 = fruit3["Name"].lower()
-    name_spice = spice["Name"].lower()
-
-    #context variable with data of database
-    context = {
-        'name_fruit1': name_fruit1,
-        'name_fruit2': name_fruit2,
-        'name_fruit3': name_fruit3,
-        'name_spice': name_spice
-    }
-    # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
